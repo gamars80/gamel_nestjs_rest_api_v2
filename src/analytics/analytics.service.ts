055a67bc58ecdf2695c3cfefbc587e4a54d15307
gamel <gamel@kimzipsa.co.kr>
2025-03-02 12:06:48 +0900

    test code

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -8,10 +8,10 @@ export class AnalyticsService {
   constructor(private readonly videoService: VideoService, private readonly emailService: EmailService) {}
 
 //   @Cron(CronExpression.EVERY_DAY_AT_10AM)
-  @Cron(CronExpression.EVERY_MINUTE)
-  async handleEmailCron() {
-    Logger.log('Email task called');
-    const videos = await this.videoService.findTop5Download();
-    this.emailService.send(videos);
-  }
+  // @Cron(CronExpression.EVERY_MINUTE)
+  // async handleEmailCron() {
+  //   Logger.log('Email task called');
+  //   const videos = await this.videoService.findTop5Download();
+  //   this.emailService.send(videos);
+  // }
 }
--- src/migrations/1738643281529-Init.ts
+++ src/migrations/1738643281529-Init.ts
@@ -4,6 +4,8 @@ export class Init1738643281529 implements MigrationInterface {
     name = 'Init1738643281529'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TYPE IF EXISTS user_role_enum`);
+        await queryRunner.query(`CREATE TYPE user_role_enum AS ENUM ('ADMIN', 'USER')`);
         await queryRunner.query(`CREATE TABLE "refresh_token" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "token" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" uuid, CONSTRAINT "REL_6bbe63d2fe75e7f0ba1710351d" UNIQUE ("user_id"), CONSTRAINT "PK_b575dd3c21fb0831013c909e7fe" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."user_role_enum" NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "video" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "mimetype" character varying NOT NULL, "download_cnt" integer NOT NULL DEFAULT '0', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" uuid, CONSTRAINT "PK_1a2f3856250765d72e7e1636c8e" PRIMARY KEY ("id"))`);
